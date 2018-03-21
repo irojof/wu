12396b612eeb21dbbdac2aaacd9c4c7aad958ec3
Rojo <irojo.f@gmail.com>
2018-03-21 13:23:55 -0300

    Agregar proyecto

---
+++ js/caribedev.js
@@ -0,0 +1,6 @@
+//*********CaribeDevelopers.com Functions v1.0*********
+//Created by caribeDevelopers.com
+//For support reasons do not change this code
+
+
+var caribedevelopers_server = "http://37.58.102.237/wus";
